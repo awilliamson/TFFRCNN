9c35d5ea89cccf98557c656b6d3a9193d1c96601
Ashley Williamson <awilliamson@lincoln.ac.uk>
2017-11-13 13:55:04 +0000

    Python 2to3 conversion
    Some work may still be required to change any remaining python 2 code.

--- lib/rpn_msr/proposal_layer_tf.py
+++ lib/rpn_msr/proposal_layer_tf.py
@@ -88,12 +88,12 @@ def proposal_layer(rpn_cls_prob_reshape, rpn_bbox_pred, im_info, cfg_key, _feat_
     #im_info = bottom[2].data[0, :]
 
     if DEBUG:
-        print 'im_size: ({}, {})'.format(im_info[0], im_info[1])
-        print 'scale: {}'.format(im_info[2])
+        print('im_size: ({}, {})'.format(im_info[0], im_info[1]))
+        print('scale: {}'.format(im_info[2]))
 
     # 1. Generate proposals from bbox deltas and shifted anchors
     if DEBUG:
-        print 'score map size: {}'.format(scores.shape)
+        print('score map size: {}'.format(scores.shape))
 
     # Enumerate all shifts
     shift_x = np.arange(0, width) * _feat_stride
